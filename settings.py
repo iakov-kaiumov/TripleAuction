cbf28aca14568ac8b96b1ed6b3507302fbbd2f53
Yasha <yashahelfi2012@gmail.com>
2023-04-17 16:35:10 +0300

    Fixed typos

--- settings.py
+++ settings.py
@@ -1,12 +1,24 @@
 from os import environ
+
+
 SESSION_CONFIG_DEFAULTS = dict(real_world_currency_per_point=1, participation_fee=0)
-SESSION_CONFIGS = [dict(name='Auction', num_demo_participants=4, app_sequence=['Introduction', 'AuctionStep', 'Results'], show_others_score=True, show_others_score_as_interval=False)]
+SESSION_CONFIGS = [
+    dict(
+        name='Auction',
+        num_demo_participants=4,
+        app_sequence=['Introduction', 'AuctionStep', 'Results'],
+        show_others_score=True,
+        show_others_score_as_interval=False
+    )
+]
+
 LANGUAGE_CODE = 'ru'
 REAL_WORLD_CURRENCY_CODE = 'USD'
 USE_POINTS = True
 DEMO_PAGE_INTRO_HTML = ''
 PARTICIPANT_FIELDS = []
 SESSION_FIELDS = []
+
 ROOMS = [dict(name='BasicRoom', display_name='BasicRoom')]
 
 ADMIN_USERNAME = 'admin'
@@ -17,5 +29,3 @@ SECRET_KEY = 'blahblah'
 
 # if an app is included in SESSION_CONFIGS, you don't need to list it here
 INSTALLED_APPS = ['otree']
-
-
